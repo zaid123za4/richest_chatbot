29f04e4c7af7a85af8cb4e48edd60ff92cd26895
zaid123za4 <zaidzaidm52@gmail.com>
2024-11-15 20:54:09 +0530

    Update index.js

--- index.js
+++ index.js
@@ -99,7 +99,8 @@ await provider.models.list().then((models) => {
 const app = express();
 const port = 10000;
 app.get('/', (req, res) => {
-  const imagePath = path.join(__dirname, 'index.html');
+  const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const imagePath = path.join(__dirname, 'index.html');
   res.sendFile(imagePath);
 });
 app.listen(port, () => {
